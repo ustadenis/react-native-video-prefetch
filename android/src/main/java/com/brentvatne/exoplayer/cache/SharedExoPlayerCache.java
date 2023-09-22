25f7d789985cde4af3b385c07906c4dc6ede4dee
Denis Ustavschikov <denis_ustavschikov@epam.com>
2023-09-22 17:30:35 +0400

    Add clear cache and update cache size methods

--- android/src/main/java/com/brentvatne/exoplayer/cache/SharedExoPlayerCache.java
+++ android/src/main/java/com/brentvatne/exoplayer/cache/SharedExoPlayerCache.java
@@ -1,6 +1,8 @@
 package com.brentvatne.exoplayer.cache;
 
 
+import static android.content.Context.MODE_PRIVATE;
+
 import android.content.Context;
 import android.util.Log;
 
@@ -16,8 +18,14 @@ public class SharedExoPlayerCache {
     private static final long exoPlayerCacheSize = 2000 * 1024 * 1024;
     private static final String TAG = "SharedExoPlayerCache";
 
+    private static final String SHARED_PREF_NAME = TAG;
+
+    private static final String SHARED_PREF_CACHE_SIZE = SHARED_PREF_NAME + "_Cache_Size";
+
     public static void initCache(Context context) {
-        LeastRecentlyUsedCacheEvictor leastRecentlyUsedCacheEvictor = new LeastRecentlyUsedCacheEvictor(exoPlayerCacheSize);
+        long cacheSize = context.getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE).getLong(SHARED_PREF_CACHE_SIZE, exoPlayerCacheSize);
+
+        LeastRecentlyUsedCacheEvictor leastRecentlyUsedCacheEvictor = new LeastRecentlyUsedCacheEvictor(cacheSize);
         StandaloneDatabaseProvider exoDatabaseProvider = new StandaloneDatabaseProvider(context);
         File cacheFolder = new File(context.getCacheDir().getAbsolutePath() + "/exoplayer/");
         Log.d(TAG, "initCache() " + cacheFolder.getAbsolutePath());
@@ -27,6 +35,17 @@ public class SharedExoPlayerCache {
     public static SimpleCache getCache() {
         Log.d(TAG, "getCache()");
         return simpleCache;
+    }
+
+    public static void releaseCache() {
+        Log.d(TAG, "releaseCache()");
+        simpleCache.release();
+    }
+
+    public static void updateCacheSize(Context context, long cacheSize) {
+        context.getSharedPreferences(SHARED_PREF_NAME, MODE_PRIVATE).edit()
+                .putLong(SHARED_PREF_CACHE_SIZE, cacheSize)
+                .apply();
     }
 
 }
